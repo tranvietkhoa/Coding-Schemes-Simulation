2ba28f330bd919db4a19f0db4b399a71ab09479b
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-12-16 00:04:42 +0700

    Integrate frontend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from "fs";
 import convolutionalRouter from './routers/convolutionalRouter.js';
 import hammingRouter from './routers/hammingRouter.js';
 import reedSolomonRouter from './routers/reedSolomonRouter.js';
@@ -20,9 +21,17 @@ exec('g++ RC_929.cpp -o RC_929', err => {
 });
 
 
+app.use(express.static('client/build'))
 app.use(convolutionalRouter);
 app.use(hammingRouter);
 app.use(reedSolomonRouter);
+
+
+app.use('*', (req, res) => {
+    fs.readFile('client/build/index.html', 'utf-8', (err, data) => {
+        res.send(data);
+    });
+});
 
 
 app.listen(process.env.PORT, () => console.log(`app listening on port ${process.env.PORT}`));
